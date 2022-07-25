f7682d46f166764b13fe6d5a43d0eadeefd6f8b2
Daria <procionestrisce@yandex.ru>
2022-07-25 18:57:59 +0300

    Готовое решение в черновике

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿void PrintArray(string[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] firstArray = { "111", "Hi", "hello", "world", "!!", "!!!!", ":)" };
+string[] responseArray = new string[firstArray.Length];
+int index = 0;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if(firstArray[i].Length <= 3)
+    {
+        responseArray[index] = firstArray[i];
+        index ++;
+    }
+    
+}
+
+Console.Write("Изначальный массив данных: ");
+PrintArray(firstArray);
+Console.WriteLine();
+Console.Write("Ответ: ");
+PrintArray(responseArray);
